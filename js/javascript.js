dfd293beb09305a1b7862d6292fe9f7ca0e14a78
Pamela Carta <noelicarta@gmail.com>
2023-03-03 20:53:10 -0300

    intento dos

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+
+function funEdu() {
+    document.getElementById("edu").style.display = 'block'
+}
+
+function funArbitro() {
+    document.getElementById("arb").style.display = 'block'
+}
+
+function funLider() {
+    document.getElementById("lider").style.display = 'block'
+}
+
+function funCont() {
+    document.getElementById("contacto").style.display = 'flex'
+}
+
